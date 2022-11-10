1ba2d05024ffbe20b35db7d7f22d82a64a045dec
aballa <aatishballa@gmail.com>
2022-11-10 13:01:12 -0600

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router"
+</script>
+
+<template>
+  <v-app>
+    <header>
+      <div class="wrapper">
+        <nav>
+          <router-link to="/" datatype="" about="">Home</router-link>
+          <router-link to="/about">About</router-link>
+        </nav>
+      </div>
+    </header>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped></style>
